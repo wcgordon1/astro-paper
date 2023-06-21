d9b0fda1e2fdad5ecca5dda78f021a5fc92732f4
satnaing <satnaingdev@gmail.com>
2023-06-21 15:32:46 +0630

    feat: add post reading time in post details

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -4,6 +4,7 @@ export const blogSchema = z
   .object({
     author: z.string().optional(),
     pubDatetime: z.date(),
+    readingTime: z.string().optional(),
     title: z.string(),
     postSlug: z.string().optional(),
     featured: z.boolean().optional(),
